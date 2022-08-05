26e9029943280f37ef43344b24d34e15999ebe5f
hassan <hassanebrahimi560@gmail.com>
2022-08-05 23:05:58 +0430

    add new list

--- packages/core/navigation/utils.ts
+++ packages/core/navigation/utils.ts
@@ -1,30 +1,49 @@
 import { map, land, collection, art, store } from "@app/navigation/assets";
-import { NavItem } from "@app/navigation/types";
+import { ListItem } from "@app/navigation/types";
 
-export const navItems: NavItem[] = [
+export const navItems: ListItem[] = [
     {
         id: 1,
-        label: "MAP",
+        label: "Map",
         icon: map,
     },
     {
         id: 2,
-        label: "LAND",
+        label: "Land",
         icon: land,
     },
     {
         id: 3,
-        label: "COLLECTION",
+        label: "Collection",
         icon: collection,
     },
     {
         id: 4,
-        label: "NFTS",
+        label: "NFTs",
         icon: art,
     },
     {
         id: 5,
-        label: "MARKETPLACE",
+        label: "Marketplace",
         icon: store,
+    },
+];
+
+export const businessMenuItems: ListItem[] = [
+    {
+        id: 1,
+        label: "About us",
+    },
+    {
+        id: 2,
+        label: "Contact us",
+    },
+    {
+        id: 3,
+        label: "Privacy Policy",
+    },
+    {
+        id: 4,
+        label: "Terms & Conditions",
     },
 ];
