b124806cecd03d9b4be9774f3bc5a1250b443162
hassan <hassanebrahimi560@gmail.com>
2022-08-05 21:19:43 +0430

    export all assets for single access point

---
+++ packages/core/navigation/assets/index.ts
@@ -0,0 +1,7 @@
+import map from "./map.svg";
+import land from "./land.svg";
+import collection from "./collection.svg";
+import art from "./art.svg";
+import store from "./store.svg";
+
+export { map, land, collection, art, store };
