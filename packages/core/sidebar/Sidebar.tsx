43f846828c4512cc4c64f7d8898e1094c5fb8c37
hassan <hassanebrahimi560@gmail.com>
2022-08-05 21:21:48 +0430

    use navigation menu

--- packages/core/sidebar/Sidebar.tsx
+++ packages/core/sidebar/Sidebar.tsx
@@ -1,5 +1,10 @@
 import styles from "./Sidebar.module.css";
+import { NavigationMenu } from "@app/navigation";
 
 export const Sidebar = () => {
-    return <aside className={styles.container}></aside>;
+    return (
+        <aside className={styles.container}>
+            <NavigationMenu />
+        </aside>
+    );
 };
