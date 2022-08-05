32e89b71c5f165f5e9664d39e94601599516726a
hassan <hassanebrahimi560@gmail.com>
2022-08-05 23:14:55 +0430

    add container for content section for limit width content

--- packages/core/footer/Footer.tsx
+++ packages/core/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { BusinessMenu, NavigationMenu } from "@app/navigation";
+import { ContactInfo } from "@app/footer/contact-info/ContactInfo";
 import { logo } from "@app/assets";
 
 import styles from "./Footer.module.css";
@@ -7,10 +8,13 @@ import styles from "./Footer.module.css";
 export const Footer = () => {
     return (
         <footer className={styles.container}>
-            <div className={styles.main}>
-                <Image alt="logo" src={logo} />
-                <NavigationMenu />
-                <BusinessMenu />
+            <div className={styles.content}>
+                <div className={styles.contentContainer}>
+                    <Image alt="logo" src={logo} />
+                    <NavigationMenu />
+                    <BusinessMenu />
+                    <ContactInfo />
+                </div>
             </div>
             <p className={styles.license}>All rights reserved © Lalaverse</p>
         </footer>
