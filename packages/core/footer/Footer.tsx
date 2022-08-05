c460f60664d454d7eb25cce79735fc8d6ab498eb
hassan <hassanebrahimi560@gmail.com>
2022-08-05 23:02:40 +0430

    implement footer

--- packages/core/footer/Footer.tsx
+++ packages/core/footer/Footer.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import Image from "next/image";
+import { BusinessMenu, NavigationMenu } from "@app/navigation";
+import { logo } from "@app/assets";
+
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
-    return <footer></footer>;
+    return (
+        <footer className={styles.container}>
+            <div className={styles.main}>
+                <Image alt="logo" src={logo} />
+                <NavigationMenu />
+                <BusinessMenu />
+            </div>
+            <p className={styles.license}>All rights reserved © Lalaverse</p>
+        </footer>
+    );
 };
