01ac894f0ebe7483f35b465987b1979e9bf40fd8
hassan <hassanebrahimi560@gmail.com>
2022-08-07 22:46:53 +0430

    and link prop for client side navigation

--- packages/core/button/Button.tsx
+++ packages/core/button/Button.tsx
@@ -1,10 +1,18 @@
-import styles from "./Button.module.css";
+import Link from "next/link";
 import { ButtonProps } from "@app/button/shared";
 
-export const Button = ({ children, onClick, type }: ButtonProps) => {
-    return (
+import styles from "./Button.module.css";
+
+export const Button = ({ children, onClick, type, link }: ButtonProps) => {
+    const button = (
         <button type={type} onClick={onClick} className={styles.button}>
             {children}
         </button>
     );
+
+    if (link) {
+        return <Link href={link}>{button}</Link>;
+    }
+
+    return button;
 };
