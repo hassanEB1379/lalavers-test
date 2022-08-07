85aa98f56435200a3bd743fb68c3a4e6c97b2081
hassan <hassanebrahimi560@gmail.com>
2022-08-07 12:37:49 +0430

    add query client to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
+import React from "react";
 import { AppPropsWithLayout } from "@app/types";
 import { AuthProvider } from "@app/authentication";
+import {
+    Hydrate,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query";
 
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const [queryClient] = React.useState(() => new QueryClient());
+
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout || (page => page);
 
     return (
-        <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <AuthProvider>
+                    {getLayout(<Component {...pageProps} />)}
+                </AuthProvider>
+            </Hydrate>
+        </QueryClientProvider>
     );
 }
 
