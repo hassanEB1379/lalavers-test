953f8328e50afa2ff00999a22fc726e2176fbdbf
hassan <hassanebrahimi560@gmail.com>
2022-08-07 18:34:37 +0430

    export useLogout from index for single access point

--- packages/lib/authentication/index.ts
+++ packages/lib/authentication/index.ts
@@ -2,3 +2,4 @@ export * from "./AuthProvider";
 export { AccessLevel } from "./AccessLevel";
 export { useSignUp } from "./state/useSignUp";
 export { useSignIn } from "./state/useSignIn";
+export { useLogout } from "./state/useLogout";
