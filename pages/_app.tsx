30fcf1f5dd35cd83736f9d7bfd8954bf767c7364
hassan <hassanebrahimi560@gmail.com>
2022-08-08 16:07:02 +0430

    remove react query config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,15 @@
 import React from "react";
 import { AppPropsWithLayout } from "@app/types";
 import { AuthProvider } from "@app/authentication";
-import {
-    Hydrate,
-    QueryClient,
-    QueryClientProvider,
-} from "@tanstack/react-query";
 
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-    const [queryClient] = React.useState(() => new QueryClient());
-
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout || (page => page);
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <Hydrate state={pageProps.dehydratedState}>
-                <AuthProvider>
-                    {getLayout(<Component {...pageProps} />)}
-                </AuthProvider>
-            </Hydrate>
-        </QueryClientProvider>
+        <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
     );
 }
 
